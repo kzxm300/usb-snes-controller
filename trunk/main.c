c2f0ac73963ae5d8b1aaa14bde8c6afdde81929e
chet2269 <chet2269@783301b0-f834-349d-062f-8e523560c16c>
2007-04-25 20:10:26 +0000

    added delay() function added code to poll SNES controller

--- trunk/main.c
+++ trunk/main.c
@@ -58,9 +58,20 @@ void high_isr( void )
 }
 
 
+/* wait a specific amount of cycles */
+/* TODO: rewrite in assembler */
+static void delay( unsigned short timeus )
+{
+  volatile unsigned short cycles;
+  
+  for ( cycles = timeus * 24; cycles > 0U; --cycles );
+}
+
 /* main entry point */
 void main( void )
 {
+  unsigned char button;  /* current button number */
+  
   ADCON1 = 0x0F; /* all pins to digital */
   LATA = 0x01; 
   TRISA = 0x00;  /* all pins to output */
@@ -82,10 +93,43 @@ void main( void )
   /* global interrupt enable */
   INTCON = 0xC0;  
   
+  /* initialization of SNES interface */
+  LATA  |= 0x02;  /* RA1 (clock) to high */
+  TRISA |= 0x08;  /* RA3 (data) to input */
+  
   while(1)
   {
-//    LATA = 1;
-//    LATA = 0;
+    /* trigger controller to latch status of all buttons */
+    /* send positive pulse on LAT, 12us */
+    LATA |= 0x04;
+    delay( 12 );
+    LATA &= ~0x04;
+    
+    /* wait 6us for controller to drive first button state */
+    delay( 6 );
+    
+    /* go over all 16 buttons */
+    for ( button = 0; button < 16U; ++button )
+    {
+      /* issue falling edge on CLK */
+      LATA &= ~0x02;
+      
+      /* sample button state from DAT */
+      if ( PORTA & 0x08 )
+      {
+        /* TODO */
+        LATA &= ~0x01;
+      }
+      
+      /* wait 6us */
+      delay( 6 );
+      
+      /* issue rising edge on CLK, controller will drive next bit */
+      LATA |= 0x02;
+
+      /* wait 6us for controller to drive next button state */
+      delay( 6 );
+    }
   }
 }
  
